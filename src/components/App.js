6c283d00ebc01dd815168e2cadc8d7de195da396
Simone Guimaraes <simoneguimaraes11@gmail.com>
2021-11-22 20:34:46 -0300

    updated confirmation page

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
-import {useState} from "react"
+import { useState } from "react";
 import Home from "./home/Home";
 import "./App.css";
 import FlightList from "./flight-list/FlightList";
-import RegistrationPage from "../components/registration-page/registration-page";
-import RegistrationEdit from "../components/registration-edit/registration-edit";
+import RegistrationPage from "./registration-page/registration-page";
+import RegistrationEdit from "./registration-edit/registration-edit";
 import FlightConfirmation from "./flight-confirmation/flight-confirmation";
 
 function App() {
@@ -16,19 +16,26 @@ function App() {
     data: "",
     qtdPessoas: 0,
   });
-  const [passagens, setPassagens] = useState([])
+  const [passagens, setPassagens] = useState([]);
   console.log(dadosVoos);
 
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home dadosVoos={dadosVoos} setDadosVoos={setDadosVoos} />} />
-        <Route path="flight-list" element={<FlightList dadosVoos={dadosVoos} setDadosVoos={setDadosVoos} />} />
+        <Route
+          path="/"
+          element={<Home dadosVoos={dadosVoos} setDadosVoos={setDadosVoos} />}
+        />
+        <Route
+          path="flight-list"
+          element={
+            <FlightList dadosVoos={dadosVoos} setDadosVoos={setDadosVoos} />
+          }
+        />
         <Route path="cadastro" element={<RegistrationPage />} />
         <Route path="editar-cadastro" element={<RegistrationEdit />} />
         <Route path="login" element="" />
-        <Route path=":id" element={<FlightConfirmation/>} />
-        <Route path="pagamento" element="" />
+        <Route path=":id" element={<FlightConfirmation />} />
         <Route path="cartao-embarque" element="" />
         <Route path="reservas" element="" />
       </Routes>
--- src/components/flight-confirmation/flight-confirmation.js
+++ src/components/flight-confirmation/flight-confirmation.js
@@ -4,7 +4,7 @@ import FlightCard from "../flight-card/flight-card";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Loading from "../loading-bar/loading";
+import CreditCardInput from "react-credit-card-input";
 
 function FlightConfirmation() {
   const flightId = useParams();
@@ -25,7 +25,7 @@ function FlightConfirmation() {
         setFlights(filtred);
         setTimeout(() => {
           setLoading(false);
-        }, 2000);
+        }, 1000);
       } catch (err) {
         console.log(err);
       }
@@ -36,9 +36,7 @@ function FlightConfirmation() {
   return (
     <div>
       <NavBar pag="Confirmação" backButton="/" />
-      {loading ? (
-        <Loading />
-      ) : flights.length === 0 ? (
+      {loading ? null : flights.length === 0 ? (
         <p className="text-center mt-5">Não existe voos para confirmar</p>
       ) : (
         <div>
@@ -54,6 +52,46 @@ function FlightConfirmation() {
             num_stops={flights[0].num_stops}
             price={flights[0].price}
           />
+          <div>
+      <CreditCardInput
+        onError={({ inputName, err }) =>
+          console.log(`credit card input error: ${err}`)
+        }
+        cardCVCInputProps={{
+          onBlur: (e) => console.log("cvc blur", e),
+          onChange: (e) => console.log("cvc change", e),
+          onError: (err) => console.log(`cvc error: ${err}`),
+        }}
+        cardExpiryInputProps={{
+          onBlur: (e) => console.log("expiry blur", e),
+          onChange: (e) => console.log("expiry change", e),
+          onError: (err) => console.log(`expiry error: ${err}`),
+        }}
+        cardNumberInputProps={{
+          onBlur: (e) => console.log("number blur", e),
+          onChange: (e) => console.log("number change", e),
+          onError: (err) => console.log(`number error: ${err}`),
+        }}
+        customTextLabels={{
+          invalidCardNumber: "O número do cartão está inválido.",
+          expiryError: {
+            invalidExpiryDate: "A data de expiração está inválida.",
+            monthOutOfRange:
+              "O mês de expiração deve ser um número entre 1 e 12.",
+            yearOutOfRange:
+              "O ano de expiração não pode ser inferior ao atual.",
+            dateOutOfRange:
+              "A data de expiração não pode ser inferior à atual.",
+          },
+          invalidCvc: "O código de segurança está inválido.",
+          invalidZipCode: "O CEP está inválido.",
+          cardNumberPlaceholder: "Número do cartão",
+          expiryPlaceholder: "MM/AA",
+          cvcPlaceholder: "CVC",
+          zipPlaceholder: "CEP",
+        }}
+      />
+    </div>
           <button className="btn-pink">Fazer o Pagamento</button>
         </div>
       )}
