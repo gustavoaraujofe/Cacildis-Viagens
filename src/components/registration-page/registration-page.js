3c8dcd75ed37096582cc5bd667838bcc90e13a93
Simone Guimaraes <simoneguimaraes11@gmail.com>
2021-11-22 15:05:45 -0300

    included link navbar

--- src/components/registration-page/registration-page.js
+++ src/components/registration-page/registration-page.js
@@ -6,59 +6,55 @@ import ButtonPink from "../button-pink/button-pink";
 import NavBar from "../navbar/NavBar";
 
 function RegistrationPage(props) {
-    const [formData, setFormData] = useState({
-      genero: "",
-      email: "",
-      password: "",
-      birthDate: "11/01/1965",
-      acceptedTerms: false,
-    });
-    const [isSending, setIsSending] = useState(false);
+  const [formData, setFormData] = useState({
+    genero: "",
+    email: "",
+    password: "",
+    birthDate: "11/01/1965",
+    acceptedTerms: false,
+  });
+  const [isSending, setIsSending] = useState(false);
 
-    function handleChange(event) {
-      setFormData({ ...formData, [event.target.name]: event.target.value });
+  function handleChange(event) {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (!formData.acceptedTerms) {
+      alert("Você precisa aceitar os Termos e Condições antes de continuar.");
+      return;
     }
 
-    function handleSubmit(event) {
-      event.preventDefault();
+    setIsSending(true);
 
-      if (!formData.acceptedTerms) {
-        alert("Você precisa aceitar os Termos e Condições antes de continuar.");
-        return;
-      }
+    axios
+      .post("https://ironrest.herokuapp.com/cacildis-viagens-voos-v2", formData)
+      .then((response) => {
+        console.log(response);
+        setIsSending(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsSending(false);
+      });
+  }
 
-      setIsSending(true);
-
-      axios
-        .post(
-          "https://ironrest.herokuapp.com/cacildis-viagens-voos-v2",
-          formData
-        )
-        .then((response) => {
-          console.log(response);
-          setIsSending(false);
-        })
-        .catch((err) => {
-          console.log(err);
-          setIsSending(false);
-        });
-    }
-
-    return (
-      <>
-        <Form
-          handleChange={handleChange}
-          handleSubmit={handleSubmit}
-          formData={formData}
-          setFormData={setFormData}
-          isSending={isSending}
-        />
-        <NavBar />
-        <Form />
-        <ButtonPink />
-      </>
-    );
-  
+  return (
+    <>
+      <Form
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        formData={formData}
+        setFormData={setFormData}
+        isSending={isSending}
+      />
+      <NavBar pag="Página de Cadastro" backButton="/" />
+      <Form />
+      <ButtonPink />
+    </>
+  );
 }
 
 export default RegistrationPage;
