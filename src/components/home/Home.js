69fd3b154d8232671d0c6cb910f15b927f5deaba
Simone Guimaraes <simoneguimaraes11@gmail.com>
2021-11-21 22:13:28 -0300

    imported flight airline pictures

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -11,15 +11,21 @@ import backgroundTwo from "../../assets/images/homepage-image-two.jpg";
 import Loading from "../loading-bar/loading";
 import UserIcon from "../user-login-icon/user-login";
 import Timer from "../timer-reservation/timer-reservation";
+import FlightCard from "../flight-card/flight-card";
+import gol from "../../assets/images/gol.jpg";
+import latam from "../../assets/images/latam.jpg";
+import azul from "../../assets/images/azul.jpg";
+import itapemerim from "../../assets/images/itapemerim.jpg";
 
 function Home() {
   return (
     <div>
+      <UserIcon />
+      <Loading />
+      <Timer />
+      <FlightCard img={[gol, latam, itapemerim, azul]} />
       <LogoTexto />
       <HomeTitle />
-      <Loading />
-      <UserIcon />
-      <Timer />
       Origem
       <InputTexto />
       Destino
